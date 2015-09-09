27026d0dba238c823324c11cb95640021c9af738
jicking <jdbebiro@gmail.com>
2015-09-09 17:44:54 +0800

    added wpf ui Signed-off-by: jicking <jdbebiro@gmail.com>

---
+++ DataApp.Wpf/ViewFactory.cs
@@ -0,0 +1,21 @@
+﻿using DataApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataApp.Wpf
+{
+    public class ViewFactory
+    {
+        public ViewInfrastructure Create()
+        {
+            User model = new User();
+            ViewModel viewModel = new ViewModel(model);
+            View view = new View();
+
+            return new ViewInfrastructure(view, viewModel, model);
+        }
+    }
+}
---
+++ DataApp.Wpf/ViewInfrastructure.cs
@@ -0,0 +1,25 @@
+﻿using DataApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataApp.Wpf
+{
+    public class ViewInfrastructure
+    {
+        public View View { get; private set; }
+
+        public ViewModel ViewModel { get; private set; }
+
+        public User Model { get; private set; }
+
+        public ViewInfrastructure(View view, ViewModel viewModel, User model)
+        {
+            this.View = view;
+            this.ViewModel = viewModel;
+            this.Model = model;
+        }
+    }
+}
